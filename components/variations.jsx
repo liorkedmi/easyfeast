a5422627b7254630dfa03c4c392417a96f7bd3cd
Lior  Kedmi <kedmi.lior@gmail.com>
2024-04-02 15:25:01 -0400

    WIP

--- components/variations.jsx
+++ components/variations.jsx
@@ -20,42 +20,46 @@ export default function Variations({ form, data }) {
           {data.map((item) => {
             const id = item.id;
 
-            if (item.fields["Variation Name"]?.length > 1) {
-              //
+            if (!item.fields["Variation Name"]) {
+              return null;
             } else {
-              const name = item.fields["Variation Name"][0];
+              if (item.fields["Variation Name"].length > 1) {
+                //
+              } else {
+                const name = item.fields["Variation Name"][0];
 
-              return (
-                <FormField
-                  key={id}
-                  control={form.control}
-                  name="variations"
-                  render={({ field }) => {
-                    return (
-                      <FormItem
-                        key={id}
-                        className="flex flex-row items-center space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <Checkbox
-                            checked={field.value?.includes(name)}
-                            onCheckedChange={(checked) => {
-                              return checked
-                                ? field.onChange([...field.value, name])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== name
-                                    )
-                                  );
-                            }}
-                          />
-                        </FormControl>
-                        <FormLabel className="font-normal">{name}</FormLabel>
-                      </FormItem>
-                    );
-                  }}
-                />
-              );
+                return (
+                  <FormField
+                    key={id}
+                    control={form.control}
+                    name="variations"
+                    render={({ field }) => {
+                      return (
+                        <FormItem
+                          key={id}
+                          className="flex flex-row items-center space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <Checkbox
+                              checked={field.value?.includes(name)}
+                              onCheckedChange={(checked) => {
+                                return checked
+                                  ? field.onChange([...field.value, name])
+                                  : field.onChange(
+                                      field.value?.filter(
+                                        (value) => value !== name
+                                      )
+                                    );
+                              }}
+                            />
+                          </FormControl>
+                          <FormLabel className="font-normal">{name}</FormLabel>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                );
+              }
             }
           })}
           <FormMessage />
